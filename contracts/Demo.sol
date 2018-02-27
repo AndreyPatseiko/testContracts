5258364652856b6a5bf8931f5507287506162600
Andrey Patseyko <andrey.patseyko@cactussoft.biz>
2018-02-27 15:05:21 +0300

    add check functions

--- contracts/Demo.sol
+++ contracts/Demo.sol
@@ -1,6 +1,10 @@
 pragma solidity ^0.4.11;
 
 contract Demo {
-    string public name = "Andrey";
+    string public name;
+
+    function changeName(string _name) {
+        name = _name;
+    }
     
 }
--- test/01_test.js
+++ test/01_test.js
@@ -10,10 +10,25 @@ contract("Test the Demo contract", function (accounts) {
     });
 
     describe("Check the contracts variables", function () {
-        it("The name variable is Andrey", function () {
-            return demoContract.name().then(function (res) {
-                expect(res.toString()).to.be.equal("Andrey");
+        it("call the function change name - Alice ", function () {
+            return demoContract.changeName('Alice').then(function (res) {
+                expect(res).to.not.be.an('error');
             });
         });
+        it('Check the variable name - should be Alice', function () {
+            return demoContract.name().then(function (res) {
+                expect(res.toString()).to.be.equal('Alice');
+            })
+        });
+        it("call the function change name - Bob ", function () {
+            return demoContract.changeName('Bob').then(function (res) {
+                expect(res).to.not.be.an('error');
+            });
+        });
+        it('Check the variable name - should be Bob', function () {
+            return demoContract.name().then(function (res) {
+                expect(res.toString()).to.be.equal('Bob');
+            })
+        })
     });
 });
